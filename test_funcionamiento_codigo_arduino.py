d7d6b29eee666af45dee92c3852df7977497803e
V1LL4 <84999700+V1LL4pro@users.noreply.github.com>
2024-05-17 18:26:15 -0500

    Update test_funcionamiento_codigo_arduino.py

--- test_funcionamiento_codigo_arduino.py
+++ test_funcionamiento_codigo_arduino.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 # Inicializa la comunicación serial
-ser = serial.Serial('COM5', 115200)  # Reemplaza 'COM5' con el puerto correcto
+ser = serial.Serial('COM14', 115200)  # Reemplaza 'COM5' con el puerto correcto
 
 while True:
     ser.write(b'\x00')  # Envia un byte en 0
@@ -16,11 +16,12 @@ while True:
             break
         
 ser.write(b'\x00') # Identificador
-ser.write(b'\x00') # Usuarios Conectados
+ser.write(b'\x01') # Usuarios Conectados
 
 while True:
     if ser.in_waiting > 0:
         incoming_byte = ser.read()  # Lee el byte entrante
-        print(f"Byte entrante: {incoming_byte}")
+        integer_value = int.from_bytes(incoming_byte, "big")  # Convierte el byte a entero
+        print(f"Byte entrante: {integer_value}")
     
 ser.close()  # Cierra la comunicación serial
